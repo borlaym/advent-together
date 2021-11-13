c112e21c89f58dfa7134d36c3f36ef41eacd485e
Marton Borlay <borlaym@gmail.com>
2021-11-13 14:19:11 +0100

    add packages

--- server/firebase.ts
+++ server/firebase.ts
@@ -1,8 +1,10 @@
 import * as admin from 'firebase-admin';
+console.log('hello')
+console.log(process.env)
 
 admin.initializeApp({
-  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_CONFIG)),
-  databaseURL: 'https://advent-together.firebaseapp.com/'
+  credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_CONFIG || '')),
+  databaseURL: 'https://advent-together-default-rtdb.europe-west1.firebasedatabase.app'
 });
 
 export default admin.database();
