b32549179023f6f8f41aa047b9847c11dc7ab172
Bodnar Istvan <istvan.bodnar@gmail.com>
2021-11-20 23:07:59 +0100

    chore: reorg

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { Calendar } from '../../types';
 import styled, { createGlobalStyle } from 'styled-components';
 
+import { HangingItem } from './HangingItem';
 import { ReactComponent as ChristmasTree } from './hanging-christmas-tree-clipart.svg';
 import { ReactComponent as ChristmasOrnament } from './simple-christmas-bulb-clipart.svg';
 
@@ -31,61 +32,6 @@ const Title = styled.h1`
   margin-top: 20vh;
   text-shadow: 2px 2px 4px hsl(145deg 61% 21%);
   color: #c8cbb9;
-`;
-
-const HangingItem = styled.div<{ inForeground?: boolean; }>`
-  // start hanging items a bit above the viewport
-  position: absolute;
-  top: -50px;
-  left: 0;
-
-  z-index: ${props => props.inForeground ? 1 : -1};
-
-  // hanging animation
-  @keyframes hanging {
-    0%   { transform: rotate(5deg) translateZ(0px); }
-    100% { transform: rotate(-5deg) translateZ(0px); }
-  }
-  animation: ${props => props.inForeground ? '2s' : '1.5s'} ease-in-out infinite alternate hanging;
-
-  // this makes it "hang" from center - top (rotation center is at x: 50%, y: -100%)
-  transform-origin: 50% -100%;
-
-  // image within the hanging container
-  & svg {
-    height: 100%;
-    width: 15vw;
-    @media screen and (max-width: 440px) {
-      width: 20vw;
-    }
-    filter: ${props => props.inForeground ? 'drop-shadow(15px 10px 13px hsl(145deg 61% 16%))' : 'drop-shadow(5px 4px 4px hsl(145deg 61% 21%))'};
-  }
-
-  // this svg does not have a rope, so one is added via :after
-  &.withRope {
-    svg {
-      margin-top: 190px;
-    }
-    :after {
-      position: absolute;
-      left: 50%; // align center
-      top: 0; // start at top of svg
-      display: block;
-      content: '';
-      width: 2px;
-      height: 190px; // same as margin above svg
-      background-color: #9b9b9b;
-    }
-
-    @media screen and (max-width: 440px) {
-      svg {
-        margin-top: 100px;
-      }
-      :after {
-        height: 100px;
-      }
-    }
-  }
 `;
 
 export default function LandingPage() {
