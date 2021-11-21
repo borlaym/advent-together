dc27389928f83bef21e620340c2d39cfd0ecacd5
Bodnár István <istvan.bodnar@gmail.com>
2021-11-21 16:15:31 +0100

    design: landing page (#3)
    * design: added new font
    
    * design: landing page design start
    
    * chore: reorg

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,16 +2,43 @@ import React, { useCallback, useState } from 'react';
 import { post } from '../../utils/api';
 import { useNavigate } from "react-router-dom";
 import { Calendar } from '../../types';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
+
+import { HangingItem } from './HangingItem';
+import { ReactComponent as ChristmasTree } from './hanging-christmas-tree-clipart.svg';
+import { ReactComponent as ChristmasOrnament } from './simple-christmas-bulb-clipart.svg';
+
+const BodyBackgroundOverride = createGlobalStyle`
+  body {
+    background-color: hsl(145deg 61% 29%);
+  }
+`;
+
+const Page = styled.div`
+  margin: 0 auto;
+  width: 80%;
+`;
 
 const Error = styled.p`
   color: red;
-`
+`;
+
+const Title = styled.h1`
+  font-family: Christmas;
+  font-size: 20vw;
+  font-weight: 200;
+  width: 100%;
+  text-align: center;
+  margin-top: 20vh;
+  text-shadow: 2px 2px 4px hsl(145deg 61% 21%);
+  color: #c8cbb9;
+`;
 
 export default function LandingPage() {
   const navigate = useNavigate();
   const [name, setName] = useState('');
   const [error, setError] = useState('');
+  const [showTitleForm, setShowTitleForm] = useState(false);
 
   const createCalendar = useCallback(() => {
     if (name.length < 3 || name.length > 100) {
@@ -22,21 +49,44 @@ export default function LandingPage() {
     })
   }, [name, navigate]);
 
+  const enableUploadForm = useCallback(() => {
+    setShowTitleForm(showTitleForm => !showTitleForm);
+  }, []);
+
   const handleNameChange = useCallback((event: React.ChangeEvent<HTMLInputElement>) => {
     setError('');
     setName(event.target.value)
   }, []);
 
   return (
-    <div>
-      <h1>Landing page</h1>
-      <input type="text" value={name} onChange={handleNameChange} /><button onClick={createCalendar}>Give me a new calendar</button>
+    <Page>
+      <BodyBackgroundOverride />
+
+      <Title>( Boldog )<br />( Karácsonyt! )</Title>
+
+      <HangingItem style={{ left: '70%', animationDelay: '0.3s' }} className="withRope">
+        <ChristmasOrnament />
+      </HangingItem>
+
+      <HangingItem inForeground={true} style={{ left: '10%' }}>
+        <ChristmasTree />
+      </HangingItem>
+
+      <p>Készíts te is saját naptárat! Hívd meg a barátaid, töltsetek fel egymásnak valamiket, aztán meg is nézhetitek WOW!</p>
+
+      {!showTitleForm && <button onClick={enableUploadForm}>Szeretnék egy saját naptárat!</button>}
+      {showTitleForm && (
+        <>
+          <input type="text" value={name} onChange={handleNameChange} /><br />
+          <button onClick={createCalendar}>Mutasd a naptáram!</button>
+        </>
+      )}
+
       {error && (
         <Error>
           {error}
         </Error>
       )}
-    </div>
+    </Page>
   )
 }
-
