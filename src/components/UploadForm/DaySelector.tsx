2f4776ab8457fd5da1a882288409524763ec3132
Bodnar Istvan <istvan.bodnar@gmail.com>
2021-11-25 02:01:15 +0100

    design: nicer(?????) upload day selector

--- src/components/UploadForm/DaySelector.tsx
+++ src/components/UploadForm/DaySelector.tsx
@@ -10,15 +10,17 @@ const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
   align-items: flex-end;
-  justify-content: flex-start;
+  justify-content: space-around;
+  max-width: 300px;
 `;
 
 const Day = styled.div`
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 0 5px;
   cursor: pointer;
+  width: 50px;
 `;
 
 const PresentsIndicator = styled.div`
@@ -26,14 +28,25 @@ const PresentsIndicator = styled.div`
   font-weight: bold;
   color: black;
   font-family: Advent;
-`
+  position: absolute;
+  top: -150%;
+  background-color: white;
+  border: 1px solid gray;
+  border-radius: 5px;
+  padding: 0.2em;
+`;
 
-const DayNumber = styled.div<{ isSelected?: boolean }>`
-  font-size: 16px;
-  color: black;
+const DayNumber = styled.div<{ isSelected?: boolean; hasPresent: boolean; }>`
+  font-size: 26px;
+  color: rgba(0 0 0 / 0.65);
 
+  ${props => props.hasPresent && css`
+    font-weight: bold;
+    color: rgba(80 80 80 / 0.4);
+  `}
   ${props => props.isSelected && css`
-    color: red;
+    // text-decoration: underline;
+    color: rgba(0 0 0 / 1);
   `}
 `;
 
@@ -66,12 +79,16 @@ export default function DaySelector({
         })();
         return (
           <Day key={i} onClick={() => onChange(i)}>
-            <PresentsIndicator>
+            {i === selectedDay && <PresentsIndicator>
               {(new Array(indicator)).fill(true).map((_, i) => (
                 <div key={i}>h</div>
               ))}
-            </PresentsIndicator>
-            <DayNumber isSelected={i === selectedDay}>{i + 1}</DayNumber>
+              {n === 0 && <span>🙁</span>}
+            </PresentsIndicator>}
+            <DayNumber
+              isSelected={i === selectedDay}
+              hasPresent={n !== 0}
+            >{i + 1}</DayNumber>
           </Day>
         );
       })}
